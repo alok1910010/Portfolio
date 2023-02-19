e22458a00fb8d9b685da03f2188f59c568cc8bd4
alok1910010 <alok1910010d@akgec.ac.in>
2023-02-19 22:51:04 +0530

    Done

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">ALOK YADAV </span>
+            from <span className="purple"> Uttar Pradesh, India.</span>
+            <br />I have done B.Tech in Computer Science Engineering From
+            <br />
+            Dr. A.P.J. Abdul Kalam Technical University, Uttar Pradesh, 
+            Some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Listening Music
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Gardening
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "There is nothing impossible to they who will try!"{" "}
+          </p>
+          <footer className="blockquote-footer">ALOK YADAV</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
